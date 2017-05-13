73817bf4b222770389d776a34c32dc27fd7c2b9d
rahul26goyal <goyal.1234rahul@gmail.com>
2017-05-13 09:56:47 +0530

    boilerplates completed

---
+++ client/index.js
@@ -0,0 +1,9 @@
+
+console.log("Hello World...!!!!")
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Welcome from './component/welcome.js';
+
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
